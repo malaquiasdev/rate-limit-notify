46d12c9d460f25145e8d7c7741e8eb22a05cd1a8
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2024-05-16 02:53:17 +0000

    test: gateway and rate limiter

---
+++ src/rate_limiter/__tests__/index.test.ts
@@ -0,0 +1,41 @@
+import { RateLimiter, IRateLimiter } from "../index";
+
+
+describe("RateLimiter", () => {
+  let mockCache: {
+    get: jest.Mock;
+    set: jest.Mock;
+  };
+  let rateLimiter: IRateLimiter;
+
+  beforeEach(() => {
+    mockCache = {
+      get: jest.fn(),
+      set: jest.fn()
+    }
+    rateLimiter = new RateLimiter(mockCache as any);
+  })
+
+  describe('STATUS', () => {
+    test('should allows request', async ()=> {
+      const key = 'mockUserId'
+      const type = 'STATUS'
+
+      mockCache.get.mockReturnValueOnce(undefined)
+      const allowed = await rateLimiter.check(key, type)
+      expect(allowed).toBeTruthy()
+    })
+    
+    test('should allows 2 requests for type STATUS', async () => {
+      const key = 'mockUserId'
+      const type = 'STATUS'
+
+      mockCache.get.mockReturnValueOnce(undefined)
+      await rateLimiter.check(key, type)
+
+      const allowed = await rateLimiter.check(key, type)
+      expect(allowed).toBeTruthy()
+      expect(mockCache.set).toHaveBeenCalledWith(key, { type, expiresAt: 120, maximum: 2, count: 2}, 120);
+    })
+  })
+})
